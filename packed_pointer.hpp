939bf9ec55384b86a34c6be1e80819e7479ef417
Gereon V <72784429+GereonV@users.noreply.github.com>
2023-09-04 22:10:10 +0200

    Improve packed_raii_pointer and add setters

--- packed_pointer.hpp
+++ packed_pointer.hpp
@@ -42,6 +42,42 @@ namespace pp {
 	template<typename PtrT, typename Traits = aligned_pointer_traits<PtrT>>
 	class packed_pointer {
 	public:
+		class int_reference {
+			friend packed_pointer;
+		public:
+			constexpr int_reference & operator=(uintptr i) const noexcept {
+				return _ref = _ref & ptr_mask() | i, *this;
+			}
+
+			constexpr operator uintptr() const noexcept {
+				return _ref & int_mask();
+			}
+		private:
+			constexpr int_reference(uintptr & v) noexcept
+				: _ref{v} {}
+
+			uintptr & _ref;
+		};
+
+		class ptr_reference {
+			friend packed_pointer;
+		public:
+			constexpr ptr_reference & operator=(PtrT p) const noexcept {
+				return _ref = _ref & int_mask() | Traits::to_int(p), *this;
+			}
+
+			constexpr operator PtrT() const
+				noexcept(noexcept(Traits::from_int(uintptr{})))
+			{
+				return Traits::from_int(_ref & ptr_mask());
+			}
+		private:
+			constexpr ptr_reference(uintptr & v) noexcept
+				: _ref{v} {}
+
+			uintptr & _ref;
+		};
+
 		constexpr packed_pointer(PtrT p, uintptr i = 0)
 			noexcept(noexcept(Traits::to_int(p)))
 			: _value{Traits::to_int(p) | i} {}
@@ -52,8 +88,31 @@ namespace pp {
 			return Traits::from_int(_value & ptr_mask());
 		}
 
+		#if __cplusplus >= 201402
+		constexpr
+		#endif
+		ptr_reference pointer() noexcept {
+			return _value;
+		}
+
 		constexpr uintptr integer() const noexcept {
 			return _value & int_mask();
+		}
+
+		#if __cplusplus >= 201402
+		constexpr
+		#endif
+		int_reference integer() noexcept {
+			return _value;
+		}
+
+		#if __cplusplus >= 201402
+		constexpr
+		#endif
+		friend void swap(packed_pointer & a, packed_pointer & b) noexcept {
+			auto const t = a._value;
+			a._value = b._value;
+			b._value = t;
 		}
 	private:
 		static constexpr uintptr int_mask() noexcept {
@@ -98,31 +157,116 @@ namespace pp {
 	template<typename PtrT>
 	using new_aligned_raii_pointer_traits = raii_pointer_traits<PtrT, __STDCPP_DEFAULT_NEW_ALIGNMENT__>;
 
-	template<typename Traits>
-	using packed_raii_pointer_base = packed_pointer<typename Traits::type *, pointer_traits<typename Traits::type *, (1 << Traits::free_bits)>>;
-
 	template<typename RAIIPtrT, typename Traits = aligned_raii_pointer_traits<RAIIPtrT>>
-	class packed_raii_pointer : public packed_raii_pointer_base<Traits> {
+	class packed_raii_pointer {
+		using pp = packed_pointer<typename Traits::type *, pointer_traits<typename Traits::type *, (1 << Traits::free_bits)>>;
 	public:
-		constexpr packed_raii_pointer(RAIIPtrT ptr, uintptr i = 0)
-			noexcept(noexcept(packed_raii_pointer_base<Traits>{Traits::release(static_cast<RAIIPtrT &&>(ptr)), i}))
-			: packed_raii_pointer_base<Traits>{Traits::release(static_cast<RAIIPtrT &&>(ptr)), i} {}
+		class int_reference : public pp::int_reference {
+			friend packed_raii_pointer;
 
-#if __cplusplus >= 202002
+			constexpr int_reference(pp & v) noexcept
+				: pp::int_reference{v.integer()} {}
+		};
+
+		class ptr_reference {
+			friend packed_raii_pointer;
+		public:
+			constexpr ptr_reference & operator=(RAIIPtrT p) const
+				noexcept(noexcept(Traits::construct(Traits::release(static_cast<RAIIPtrT &&>(p)))))
+			{
+				return Traits::construct(_ref.pointer()),
+				       _ref.pointer() = Traits::release(static_cast<RAIIPtrT &&>(p)),
+				       *this;
+			}
+
+			constexpr operator typename Traits::type *() const
+				noexcept(noexcept(_ref.pointer()))
+			{
+				return _ref.pointer();
+			}
+		private:
+			constexpr ptr_reference(pp & v) noexcept
+				: _ref{v} {}
+
+			pp & _ref;
+		};
+
+		constexpr packed_raii_pointer(RAIIPtrT ptr, uintptr i = 0)
+			noexcept(noexcept(Traits::release(static_cast<RAIIPtrT &&>(ptr))))
+			: _pp{Traits::release(static_cast<RAIIPtrT &&>(ptr)), i} {}
+
+		constexpr packed_raii_pointer(packed_raii_pointer const & other)
+			noexcept(noexcept(
+				packed_raii_pointer{static_cast<RAIIPtrT const &>(
+					Traits::construct(other.pointer())
+				)}
+			))
+			: packed_raii_pointer{static_cast<RAIIPtrT const &>(
+				Traits::construct(other.pointer())
+			), other.integer()} {}
+
+		#if __cplusplus >= 201402
 		constexpr
-#endif
+		#endif
+		packed_raii_pointer(packed_raii_pointer && other)
+			noexcept(noexcept(
+				Traits::release(Traits::construct(other.pointer()))
+			))
+			: _pp{nullptr}
+		{
+			auto t = Traits::construct(other.pointer());
+			_pp = pp{Traits::release(static_cast<RAIIPtrT &&>(t)), other.integer()};
+			other._pp = pp{Traits::release(static_cast<RAIIPtrT &&>(t))};
+		}
+
+		#if __cplusplus >= 202002
+		constexpr
+		#endif
 		~packed_raii_pointer() {
 			Traits::construct(this->pointer());
 		}
+
+		constexpr typename Traits::type * pointer() const
+			noexcept(noexcept(_pp.pointer()))
+		{
+			return _pp.pointer();
+		}
+
+		#if __cplusplus >= 201402
+		constexpr
+		#endif
+		ptr_reference pointer() noexcept {
+			return _pp;
+		}
+
+		constexpr uintptr integer() const noexcept {
+			return _pp.integer();
+		}
+
+		#if __cplusplus >= 201402
+		constexpr
+		#endif
+		int_reference integer() noexcept {
+			return _pp;
+		}
+
+		#if __cplusplus >= 201402
+		constexpr
+		#endif
+		friend void swap(packed_raii_pointer & a, packed_raii_pointer & b) noexcept {
+			swap(a._pp, b._pp);
+		}
+	private:
+		pp _pp;
 	};
 
-#if __cplusplus >= 201703
+	#if __cplusplus >= 201703
 	template<typename PtrT>
 	packed_raii_pointer(PtrT) -> packed_raii_pointer<PtrT>;
 
 	template<typename PtrT, typename Int>
 	packed_raii_pointer(PtrT, Int) -> packed_raii_pointer<PtrT>;
-#endif
+	#endif
 
 } // pp
 } // hdrs
